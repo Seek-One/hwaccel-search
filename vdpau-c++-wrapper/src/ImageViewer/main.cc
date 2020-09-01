f4821b4498832fb7f9de59d754bac1f0d32e6e66
Arthur HUGEAT <arthur.hugeat@jet1oeil.com>
2020-09-01 10:52:40 +0200

    [VDPAU wrapper] Add options for ImageViewer

--- vdpau-c++-wrapper/src/ImageViewer/main.cc
+++ vdpau-c++-wrapper/src/ImageViewer/main.cc
@@ -28,22 +28,109 @@
 #include <VdpWrapper/DecodedSurface.h>
 #include <VdpWrapper/VideoMixer.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        std::cerr << "Incorrect parameter" << std::endl;
-        std::cerr << "Usage" << std::endl;
-        std::cerr << "\t" << std::string(argv[0]) << " IMAGE_FILE" << std::endl;
+namespace {
+    void printUsage(const std::string& commandName, const std::string& message) {
+        std::cerr << message << std::endl;
+        std::cerr << "Usage:" << std::endl;
+        std::cerr << "\t" << commandName << " [OPTION] --image-size <width>x<height> RAW_IMAGE_FILE" << std::endl;
+        std::cerr << std::endl;
+        std::cerr << "Options:" << std::endl;
+        std::cerr << "\t--image-size <width>x<height>\t\tSet the source image size" << std::endl;
+        std::cerr << "\t--initial-size <width>x<height>\t\tSet the initial screen size" << std::endl;
+    }
+}
 
+int main(int argc, char *argv[]) {
+    int iCurrentArg = 1;
+    vw::SizeU screenSize(1280, 720);
+    vw::SizeU sourceSize(0, 0);
+
+    while (iCurrentArg < argc - 1) {
+        std::string szArg = std::string(argv[iCurrentArg]);
+        if (szArg == "--image-size") {
+            bool bOptionParseFailed = false;
+            std::string szWidth;
+            std::string szHeight;
+
+            if (iCurrentArg >= argc - 1) {
+                bOptionParseFailed = true;
+            }
+            else {
+                std::string szValue = std::string(argv[iCurrentArg + 1]);
+                int iDelimiterIndex = szValue.find_first_of("x");
+                szWidth = szValue.substr(0, iDelimiterIndex);
+                szHeight = szValue.substr(iDelimiterIndex + 1);
+
+                try {
+                    int iWidth = std::stoi(szWidth);
+                    int iHeight = std::stoi(szHeight);
+
+                    sourceSize = vw::SizeU(iWidth, iHeight);
+                } catch (std::invalid_argument &e) {
+                    bOptionParseFailed = true;
+                }
+            }
+
+            if (bOptionParseFailed) {
+                printUsage(argv[0], "Wrong size values");
+                return 1;
+            }
+
+            std::cout << "[main] Set source image size: " << szWidth << "x" << szHeight << std::endl;
+            iCurrentArg += 2;
+        } else if (szArg == "--initial-size") {
+            bool bOptionParseFailed = false;
+            std::string szWidth;
+            std::string szHeight;
+
+            if (iCurrentArg >= argc - 1) {
+                bOptionParseFailed = true;
+            }
+            else {
+                std::string szValue = std::string(argv[iCurrentArg + 1]);
+                int iDelimiterIndex = szValue.find_first_of("x");
+                szWidth = szValue.substr(0, iDelimiterIndex);
+                szHeight = szValue.substr(iDelimiterIndex + 1);
+
+                try {
+                    int iWidth = std::stoi(szWidth);
+                    int iHeight = std::stoi(szHeight);
+
+                    screenSize = vw::SizeU(iWidth, iHeight);
+                } catch (std::invalid_argument &e) {
+                    bOptionParseFailed = true;
+                }
+            }
+
+            if (bOptionParseFailed) {
+                printUsage(argv[0], "Wrong size values");
+                return 1;
+            }
+
+            std::cout << "[main] Set initial screen size: " << szWidth << "x" << szHeight << std::endl;
+            iCurrentArg += 2;
+        } else {
+            printUsage(argv[0], "'" + szArg + "' unknown option");
+            return 1;
+        }
+    }
+
+    if (iCurrentArg != argc - 1) {
+        printUsage(argv[0], "Missing parameter");
         return 1;
     }
 
-    vw::SizeU screenSize(1280, 720);
-    vw::SizeU sourceSize(1920, 1080);
+    if (sourceSize == vw::SizeU(0u, 0u)) {
+        printUsage(argv[0], "The source image size must be defined");
+        return 1;
+    }
+
+    std::string szRawImageFile(argv[iCurrentArg]);
 
     vw::Display display(screenSize);
     vw::Device device(display);
     vw::PresentationQueue presentationQueue(display, device);
-    vw::DecodedSurface inputSurface(device, argv[1], sourceSize);
+    vw::DecodedSurface inputSurface(device, szRawImageFile, sourceSize);
     vw::VideoMixer mixer(device, screenSize);
 
     while (display.isOpened()) {
